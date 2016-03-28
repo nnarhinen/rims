44a77e56bc437413a9e5ee3bb61912c2edfadc36
Niklas Närhinen <niklas@narhinen.net>
2016-03-29 02:28:01 +0300

    First steps creating project structure

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import test from 'ava';
 import run from 'test-cli';
 import cli from './lib/cli';
+import tmp from 'tmp';
+import fs from 'fs';
 
 const rims = run.bind(null, cli);
 
@@ -20,3 +22,26 @@ test.cb('rims help, list commands', t => {
     t.end();
   });
 });
+
+test.cb('rims new, create new project', t => {
+  t.plan(2);
+  withTempDirectory((err, rims, path) => {
+    if (err) return t.end(err);
+    rims('new', (stdout, stderr, code) => {
+      t.is(code, 0);
+      fs.readdir(path, (err, files) => {
+        t.ok(files.indexOf('shared') !== -1);
+        t.end();
+      });
+    });
+  });
+});
+
+function withTempDirectory(cb) {
+  tmp.dir((err, path) => {
+    if (err) cb(err);
+    cb(null, run.bind(null, cli, (process) => {
+      process.cwd = () => path;
+    }), path);
+  });
+}
